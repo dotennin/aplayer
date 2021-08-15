c446cd39c721d3449b61b208411f0a7d17ae2bba
Wenhua.Chen <wenhua.chen@asurion.com>
2021-08-15 16:42:34 +0000

    feat: service worker api cache fetch

--- mini-player/serviceWorker.js
+++ mini-player/serviceWorker.js
@@ -16,4 +16,18 @@ self.addEventListener("install", installEvent => {
 })
 
 // this will resolve the error 'page does not work offline'
-self.addEventListener('fetch', function(event) {});
+// self.addEventListener('fetch', function(event) {});
+
+// Fetching content using Service Worker
+self.addEventListener('fetch', (e) => {
+  e.respondWith((async () => {
+    const r = await caches.match(e.request);
+    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+    if (r) return r;
+    const response = await fetch(e.request);
+    const cache = await caches.open(cacheName);
+    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+    cache.put(e.request, response.clone());
+    return response;
+  })());
+});
