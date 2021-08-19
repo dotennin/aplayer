a1009b92c9289b120a1b1d84d3477aa296e25c4a
Wenhua.Chen <wenhua.chen@asurion.com>
2021-08-19 22:02:05 +0900

    feat: path api

--- api/main.go
+++ api/main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
+	"os"
+	"sort"
+	"strings"
+	"time"
 )
 
-type Profile struct {
+type FileInfo struct {
+	Name string
+	Size int64
+	Mode os.FileMode
+	ModTime time.Time
+	IsDir bool
+	Path string
+	Sys interface{}
+}
+
+type PathQuery struct {
 	Name string
 }
 
@@ -27,34 +43,41 @@ func authorize(w http.ResponseWriter, r *http.Request) {
 	setHeader(w, data, err)
 }
 
-func ignore(w http.ResponseWriter, r *http.Request) {
-	data, err := ioutil.ReadFile("./json/ignore.json")
-	setHeader(w, data, err)
-}
-func productCount(w http.ResponseWriter, r *http.Request) {
-	data, err := ioutil.ReadFile("./json/productCount.json")
-	setHeader(w, data, err)
-}
-
 func getPath(w http.ResponseWriter, r *http.Request) {
-		files, err := ioutil.ReadDir("./")
+		decodedValue, err := url.QueryUnescape(r.URL.RawQuery)
+		queryParams := strings.Split(decodedValue, "&")
+		sort.Strings(queryParams)
+
+		var pathQuery PathQuery
+		for _, queryParam :=range queryParams {
+			params := strings.Split(queryParam, "=")
+			if strings.ToLower(params[0]) == "name" {
+				pathQuery.Name = params[1]
+			}
+		}
+
+		dir := "/mp3"
+		if pathQuery.Name != "" {
+			dir += "/" + pathQuery.Name
+		}
+		files, err := ioutil.ReadDir(dir)
+ 
+		var fileInfoList []FileInfo
+    for _, f := range files {
+			fileInfo := FileInfo{Name: f.Name(), Size: f.Size(), Mode: f.Mode(), ModTime: f.ModTime(), IsDir: f.IsDir(), Path: pathQuery.Name + "/" + f.Name()}
+			fileInfoList = append(fileInfoList, fileInfo)
+    }
+
+    b, err := json.Marshal(fileInfoList)
     if err != nil {
         log.Fatal(err)
-				log.Fatal(files)
     }
- 
-		var dataString string
-    for _, f := range files {
-			dataString += f.Name() + " d"
-    }
-		setHeader(w, []byte(dataString), err)
+		setHeader(w, b, err)
 }
 
 func main() {
 	http.HandleFunc("/api/authorize", authorize)
-	http.HandleFunc("/api/ignore", ignore)
-	http.HandleFunc("/api/product_count", productCount)
-	http.HandleFunc("/api/getPath", getPath)
+	http.HandleFunc("/api/path", getPath)
 
 
 	log.Fatal(http.ListenAndServe(":80", nil))
